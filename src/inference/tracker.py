f35d51a456c88976dd991dffb6f6f32b1fae5e6c
mhashirshahid <hashirshahid1119@gmail.com>
2026-05-10 21:36:11 +0500

    fix: resolve AttributeError by updating tracker to access Detection dataclass attributes

--- src/inference/tracker.py
+++ src/inference/tracker.py
@@ -7,17 +7,35 @@ class SimpleTracker:
         updated_tracks=[]
         for det in detections:
             matched=False
+            
+            # Reconstruct [x1, y1, x2, y2] for the new Detection dataclass
+            det_box = [
+                det.x_center - (det.width / 2),
+                det.y_center - (det.height / 2),
+                det.x_center + (det.width / 2),
+                det.y_center + (det.height / 2)
+            ]
+
             for track in self.active_tracks:
-                if(self._iou(det['bbox'],track['bbox'])>0.3):
-                    det['track_id']=track['track_id']
+                track_box = [
+                    track.x_center - (track.width / 2),
+                    track.y_center - (track.height / 2),
+                    track.x_center + (track.width / 2),
+                    track.y_center + (track.height / 2)
+                ]
+
+                if(self._iou(det_box,track_box)>0.3):
+                    det.track_id=track.track_id
                     updated_tracks.append(det)
                     self.active_tracks.remove(track)
                     matched=True
                     break
+                    
             if(not matched):
-                det['track_id']=self.next_id
+                det.track_id=self.next_id
                 self.next_id+=1
                 updated_tracks.append(det)
+                
         self.active_tracks=updated_tracks
         return self.active_tracks
 
